aaaac73c15d04c2abeb71e0bceeffb191547b4d3
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-28 09:17:49 -0500

    Add comments

--- lexer.ml
+++ lexer.ml
@@ -24,6 +24,10 @@ let rec lex = parser
       Buffer.add_char buffer c;
       lex_number buffer stream
 
+  (* comment /#.*$/ *)
+  | [< ' ('#'); stream >] ->
+      lex_comment stream
+
 and lex_ident buffer = parser
   | [< ' ('A' .. 'Z' | 'a' .. 'z' | '0' .. '9' as c); stream >] ->
       Buffer.add_char buffer c;
@@ -40,3 +44,8 @@ and lex_number buffer = parser
       lex_number buffer stream
   | [< stream=lex >] ->
       [< 'Token.Number (float_of_string (Buffer.contents buffer)); stream >]
+
+and lex_comment = parser
+  | [< ' ('\n'); stream=lex >] -> stream
+  | [< 'c; e=lex_comment >] -> e
+  | [< >] -> [< >]
