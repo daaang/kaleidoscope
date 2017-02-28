aaaaea358ffbd3bd8de9b75b167016f2eb6b37e1
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-28 09:55:50 -0500

    Add variables, binary operators, and function calls

--- ast.ml
+++ ast.ml
@@ -1,3 +1,12 @@
 type expr =
   (* variant for numeric literals, e.g. "1.0" *)
   | Number of float
+
+  (* variant for referencing a variable, e.g. "a" *)
+  | Variable of string
+
+  (* variant for a binary operator, e.g. "+" *)
+  | Binary of char * expr * expr
+
+  (* variant for function calls *)
+  | Call of string * expr array
