aaaacb08eddf1bfd1cb9ba9928bb942064879b2c
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-28 09:11:49 -0500

    Add numbers

--- lexer.ml
+++ lexer.ml
@@ -12,11 +12,17 @@ let rec lex = parser
   (* skip whitespace *)
   | [< ' (' ' | '\n' | '\r' | '\t'); stream >] -> lex stream
 
-  (* commands and idents *)
+  (* commands and idents /[A-Za-z][A-Za-z0-9]*/ *)
   | [< ' ('A' .. 'Z' | 'a' .. 'z' as c); stream >] ->
       let buffer = Buffer.create 1 in
       Buffer.add_char buffer c;
       lex_ident buffer stream
+
+  (* number /[0-9][0-9.]*/ *)
+  | [< ' ('0' .. '9' as c); stream >] ->
+      let buffer = Buffer.create 1 in
+      Buffer.add_char buffer c;
+      lex_number buffer stream
 
 and lex_ident buffer = parser
   | [< ' ('A' .. 'Z' | 'a' .. 'z' | '0' .. '9' as c); stream >] ->
@@ -27,3 +33,10 @@ and lex_ident buffer = parser
       | "def" -> [< 'Token.Def; stream >]
       | "extern" -> [< 'Token.Extern; stream >]
       | id -> [< 'Token.Ident id; stream >]
+
+and lex_number buffer = parser
+  | [< ' ('0' .. '9' | '.' as c); stream >] ->
+      Buffer.add_char buffer c;
+      lex_number buffer stream
+  | [< stream=lex >] ->
+      [< 'Token.Number (float_of_string (Buffer.contents buffer)); stream >]
