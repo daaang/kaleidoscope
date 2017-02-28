aaaafaeb77a5843777419f6a0a9bdcec6a15cfb7
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-28 09:05:34 -0500

    Add commands and idents

--- lexer.ml
+++ lexer.ml
@@ -11,3 +11,19 @@ type token =
 let rec lex = parser
   (* skip whitespace *)
   | [< ' (' ' | '\n' | '\r' | '\t'); stream >] -> lex stream
+
+  (* commands and idents *)
+  | [< ' ('A' .. 'Z' | 'a' .. 'z' as c); stream >] ->
+      let buffer = Buffer.create 1 in
+      Buffer.add_char buffer c;
+      lex_ident buffer stream
+
+and lex_ident buffer = parser
+  | [< ' ('A' .. 'Z' | 'a' .. 'z' | '0' .. '9' as c); stream >] ->
+      Buffer.add_char buffer c;
+      lex_ident buffer stream
+  | [< stream=lex >] ->
+      match Buffer.contents buffer with
+      | "def" -> [< 'Token.Def; stream >]
+      | "extern" -> [< 'Token.Extern; stream >]
+      | id -> [< 'Token.Ident id; stream >]
